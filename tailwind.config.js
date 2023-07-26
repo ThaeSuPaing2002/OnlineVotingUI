52208e5a3b225aac3a682de3d86b974a3f90cbc1
ThaeSuPaing2002 <pit6837@gmail.com>
2023-07-26 20:03:54 +0630

    modify screen breakpoint in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1040px",
+      xxl: "1440px",
+    },
+    extend: {
+
+    },
   },
   plugins: [require("daisyui")],
 }
